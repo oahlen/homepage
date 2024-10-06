df9fa63be05d55399178a1391479b6fe473c3a56
Oscar Ahlén <github@ahlen.dev>
2024-10-06 15:28:27 +0200

    Update flake

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "My personal homepage and resume";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
